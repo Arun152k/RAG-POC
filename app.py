a917e9dd9dacfbf794394b8b219383a464064207
Arun152k <rarunkumar15112k@gmail.com>
2024-06-29 11:29:21 -0700

    Updated requirements.txt

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-try:
-    __import__("pysqlite3")
-    import sys
+import pysqlite3
+import sys
 
-    sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
-except ImportError:
-    pass
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
 import os
 import openai
